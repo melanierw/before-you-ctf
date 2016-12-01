fb0722f9a9066d1520aafff8650d865b4c0400b9
Vagrant Default User <vagrant@debiancontrib-jessie.vagrantup.com>
2016-12-01 19:42:10 +0000

    source/crawl getting site up for viewing

---
+++ byc-problems/source/challenge.py
@@ -0,0 +1,13 @@
+from hacksport.problem import Challenge, File
+from hacksport.operations import execute
+import string
+import os
+
+class Problem(Challenge):
+	#comment
+	def setup(self):
+		x = 1 + 1
+		files = [File("core1")]
+	def generate_flag(self,random):
+		m = "svchost.exe"
+		return m
