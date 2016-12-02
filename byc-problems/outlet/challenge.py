9a2e54fb5eaac5f1c4775598a4fb78f27f3c3723
Vagrant Default User <vagrant@debiancontrib-jessie.vagrantup.com>
2016-12-02 01:27:12 +0000

    Hilarious Outlet built, TODO update picture with GPS

---
+++ byc-problems/outlet/challenge.py
@@ -0,0 +1,13 @@
+from hacksport.problem import Challenge, File
+from hacksport.operations import execute
+import string
+import os
+
+class Problem(Challenge):
+	#comment
+	def setup(self):
+		x = 1 + 1
+		files = [File("what_picture.jpg")]	
+
+	def generate_flag(self,random):
+		return "zion"
